5047cc4c3289588c69c478a39ba72978a72b0792
Nicolas Riesco <riesco@gmail.com>
2012-07-19 00:21:09 -0400

    a
    a

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-sublime
+Cambios cambios
 =======
 
 sublime
--- borrar.php
+++ borrar.php
@@ -5,6 +5,10 @@
 
 // ahora estoy en bbb y hago cambios
 echo 123;
+<<<<<<< HEAD
 >>>>>>> cambios en bbb
+=======
+// cambio de branch
+>>>>>>> a
 
 ?>
