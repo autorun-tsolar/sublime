6bf5bddd00fb973539b15826626cd327edc6f045
Nicolas Riesco <riesco@gmail.com>
2012-07-22 22:43:23 -0400

    test

--- manage/segundo.php
+++ manage/segundo.php
@@ -1,5 +1,5 @@
 <?php
-// segundo archivo de prueba OTRO CAMBIO
+// segundo archivo de prueba OTRO CAMBIO 3
 echo "Esto si va a funcionar!";
 //y cambiando desde sublime para probar
 // editando desde la consola de github
